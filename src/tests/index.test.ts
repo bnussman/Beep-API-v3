bac4945fb1ec5018c610f70f06c76ee2fd65d340
Banks Nussman <banks@nussman.us>
2020-09-07 00:09:32 -0400

    fixed test not terminating

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,8 +1,3 @@
-import BeepAPIServer from "../index";
-
-//make a beep api object to the tests can acually run aginst the server
-new BeepAPIServer();
-
 describe("Sign Up", () => {
     test("create an account", () => {
         expect(1).toBe(1);
