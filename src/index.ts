95733824360e2eeba21737c15a4422fbf49ab010
Banks Nussman <banks@nussman.us>
2020-09-19 17:17:58 -0400

    added Sentry express features

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Application } from 'express';
-import express = require('express');
+import * as express from 'express';
 import * as Auth from "./auth/routes";
 import * as Account from "./account/routes";
 import * as Rider from "./rider/routes";
 import * as Beeper from "./beeper/routes";
 import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 export default class BeepAPIServer {
 
@@ -12,16 +13,22 @@ export default class BeepAPIServer {
     private port: number;
 
     constructor() {
-        this.initializeSentry();
         this.app = express();
+        this.initializeSentry(this.app);
         this.port = 3001;
         this.setFeatures();
         this.setEndpoints();
     }
 
-    initializeSentry(): void {
+    initializeSentry(app: Application): void {
         Sentry.init({
             dsn: "https://1588498392e8472697b3c496de6a3a53@sentry.nussman.us/2",
+            integrations: [
+                // enable HTTP calls tracing
+                new Sentry.Integrations.Http({ tracing: true }),
+                // enable Express.js middleware tracing
+                new Tracing.Integrations.Express({ app }),
+            ],
             tracesSampleRate: 1.0,
         });
     }
@@ -37,6 +44,9 @@ export default class BeepAPIServer {
         this.app.use(express.json())
         this.app.use(express.urlencoded({ extended: true }))
         this.app.disable('x-powered-by');
+        this.app.use(Sentry.Handlers.requestHandler());
+        this.app.use(Sentry.Handlers.tracingHandler());
+        this.app.use(Sentry.Handlers.errorHandler());
     }
 
     start(): void {
