4e2a36e19c49ae8f18ca87a3c39f572773dd2708
Banks Nussman <banks@nussman.us>
2020-09-23 20:47:03 -0400

    now using correct env name for sentry

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -6,7 +6,7 @@ export function initializeSentry(app: Application): void {
     console.log(process.env);
     Sentry.init({
         dsn: process.env.SENTRY_URL || "http://ddeca23af15c47a7819d89a0d92e3d68@192.168.1.124:9000/2",
-        environment: process.env.CI_ENVIRONMENT_NAME || "development",
+        environment: process.env.GITLAB_ENVIRONMENT_NAME || "development",
         integrations: [
             // enable HTTP calls tracing
             new Sentry.Integrations.Http({ tracing: true }),
