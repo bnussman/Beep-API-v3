3a57a2fba38fb42f57b0b479343f880a9fc1d8ac
Banks Nussman <banks@nussman.us>
2020-09-23 20:13:01 -0400

    Added Sentry envrionment names

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -5,6 +5,7 @@ import { Application } from "express";
 export function initializeSentry(app: Application): void {
     Sentry.init({
         dsn: process.env.SENTRY_URL || "http://ddeca23af15c47a7819d89a0d92e3d68@192.168.1.124:9000/2",
+        environment: process.env.CI_ENVIRONMENT_NAME || "development",
         integrations: [
             // enable HTTP calls tracing
             new Sentry.Integrations.Http({ tracing: true }),
