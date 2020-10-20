1f5fbc7831570b9774a89a08fb61f5080eac9efa
Banks Nussman <banks@nussman.us>
2020-10-20 15:39:14 -0400

    added cors

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import { RegisterRoutes } from "../build/routes";
 import { connect } from "./utils/db";
 //import { errorHandler } from "./utils/Error";
 import { handleNotFound } from "./utils/404";
+import cors from "cors";
 
 connect();
 
 export const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
