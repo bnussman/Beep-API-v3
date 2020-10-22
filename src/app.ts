7f37ceb66e89becf2e8af251738d8475ca63e49b
Banks Nussman <banks@nussman.us>
2020-10-22 00:35:14 -0400

    readded sentry use by express

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import { RegisterRoutes } from "../build/routes";
 import { connect } from "./utils/db";
 import { errorHandler } from "./utils/Error";
 import { handleNotFound } from "./utils/404";
+import * as Sentry from "@sentry/node";
+import { initializeSentry } from "./utils/sentry";
 import cors from "cors";
 
 connect();
@@ -13,7 +15,17 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+initializeSentry(app);
+
+app.use(Sentry.Handlers.requestHandler({
+    user: ["id"]
+}));
+
+app.use(Sentry.Handlers.tracingHandler());
+
 RegisterRoutes(app);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(handleNotFound);
 
--- src/docs/routes.ts
+++ src/docs/routes.ts
@@ -8,8 +8,6 @@ export class DocsController extends Controller {
     public getDocs(): any {
         this.setStatus(200);
         const data = fs.readFileSync('build/swagger.json');
-        const a = data.toString();
-        const b = JSON.parse(a);
-        return b;
+        return JSON.parse(data.toString());
     }
 }
