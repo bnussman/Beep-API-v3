6d9333c9959c38537bb2f8054b6d64b16aa81b59
Banks Nussman <banks@nussman.us>
2021-01-04 21:00:08 -0500

    updated directions endpoint

--- src/directions/routes.ts
+++ src/directions/routes.ts
@@ -5,6 +5,7 @@ import fetch from 'node-fetch';
 @Tags("Directions")
 @Route("directions")
 export class DirectionsController extends Controller {
+
     @Security("token")
     @Get("{start}/{end}")
     public async getDirections(@Path() start: string, @Path() end: string): Promise<any | APIResponse> {
@@ -12,12 +13,23 @@ export class DirectionsController extends Controller {
             const result = await fetch('https://maps.googleapis.com/maps/api/directions/json?origin=' + start + '&destination=' + end + '&key=AIzaSyBgabJrpu7-ELWiUIKJlpBz2mL6GYjwCVI');
             const data = await result.json();
 
-            return {
-                status: APIStatus.Success,
-                eta: data.routes[0].legs[0].duration.text
-            };
+            this.setStatus(result.status);
+
+            if (result.ok) {
+                return {
+                    status: APIStatus.Success,
+                    eta: data.routes[0].legs[0].duration.text
+                };
+            }
+            else {
+                return {
+                    status: APIStatus.Error,
+                    message: data.error_message
+                };
+            }
         }
         catch (error) {
+            this.setStatus(500);
             return {
                 status: APIStatus.Error,
                 message: error
