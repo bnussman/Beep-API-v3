405b1fbf78cb9892b7ebbf089bd79ec56d03c4fa
Banks Nussman <banks@nussman.us>
2021-01-15 13:42:53 -0500

    fixed null

--- src/reports/routes.ts
+++ src/reports/routes.ts
@@ -309,10 +309,11 @@ export class ReportsController extends Controller {
     public async getReport(@Path() id: string): Promise<ReportResponse | APIResponse> {
         try {
             const result = await r.table("userReports").get(id).merge(function(doc: any) {
+                console.log(doc('handledBy').args);
                 return {
                     reporter: r.table('users').get(doc('reporterId')).without(...withoutsArr),
                     reported: r.table('users').get(doc('reportedId')).without(...withoutsArr),
-                    handledByUser: !(doc('handledBy').isEmpty()) ? r.table('users').get(doc('handledBy')).without(...withoutsArr) : null
+                    handledByUser: r.table('users').get(doc('handledBy')).without(...withoutsArr).default(null)
                 };
             }).without('reportedId', 'reporterId', 'handledBy').run((await database.getConn()));
             console.log(result);
