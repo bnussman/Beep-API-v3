8110665fd34f6900b67704518103ef5ed541b56e
Banks Nussman <banks@nussman.us>
2021-02-26 14:48:49 -0500

    renamed getETa

--- src/directions/resolver.ts
+++ src/directions/resolver.ts
@@ -5,7 +5,7 @@ import { Arg, Query, Resolver } from "type-graphql";
 export class DirectionsResolver {
 
     @Query(() => String)
-    public async getDirections(@Arg('start') start: string, @Arg('end') end: string): Promise<string> {
+    public async getETA(@Arg('start') start: string, @Arg('end') end: string): Promise<string> {
         const result = await fetch('https://maps.googleapis.com/maps/api/directions/json?origin=' + start + '&destination=' + end + '&key=AIzaSyBgabJrpu7-ELWiUIKJlpBz2mL6GYjwCVI');
 
         const data = await result.json();
